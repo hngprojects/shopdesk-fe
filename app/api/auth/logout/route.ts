266934e3995807b6ae1a0e9331b0fec8bdf9719a
Siddami <dharmmy621@gmail.com>
2025-03-20 12:00:09 +0100

    fix: add logout functionality
    - Add logout route from auth.ts files
    - clear token from cookies
    -navigate user back to homepage on successful logout

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,24 +1,36 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from "next/server"; 
+import { cookies } from "next/headers";
 
-export async function POST(req: Request) {
+export async function POST() {
   try {
-    const { refresh_token } = await req.json();
+    const cookieStore = await cookies(); 
+
+    const refresh_token = cookieStore.get("refresh_token")?.value;
+    const access_token = cookieStore.get("access_token")?.value;
 
     if (!refresh_token) {
-      return NextResponse.json({ message: "Refresh token is required" }, { status: 400 });
+      return NextResponse.json({ message: "Refresh token is missing" }, { status: 400 });
+    }
+    if (!access_token) {
+      return NextResponse.json({ message: "Access token is missing" }, { status:400 });
     }
 
     const response = await fetch("https://api.timbu.cloud/auth/logout", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ refresh_token }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${refresh_token}`,
+      },
     });
 
     const data = await response.json();
 
     if (!response.ok) {
-      throw new Error(data.message || "Logout failed");
+      console.log(data.message, data.detail , "Logout failed");
     }
+
+    await cookieStore.delete("refresh_token"); 
+    await cookieStore.delete("access_token");
 
     return NextResponse.json({ message: "User logged out successfully" });
   } catch (error: any) {
