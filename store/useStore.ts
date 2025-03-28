1d033a1583f0ab75be1000d5b08387d7850c5549
Shina-Kelani Taiwo <kelanitaiwo34@gmail.com>
2025-03-28 09:35:55 +0100

    update: store

--- store/useStore.ts
+++ store/useStore.ts
@@ -1,5 +1,6 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import type { StockItem } from '@/types/stocks';
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 type Product = {
   id: string;
@@ -23,15 +24,21 @@ type State = {
   organizationName: string;
   organizationInitial: string;
   products: Product[];
+  stockItems: StockItem[];
+  isPremium: boolean;
   isProductLoading: boolean;
   revenueData: number[];
   isRevenueLoading: boolean;
   isRevenueError: boolean;
   metrics: Metric[];
   isMetricsLoading: boolean;
+  isSearching: boolean;
+  searchText: string;
   setOrganizationId: (organizationId: string) => void;
   setOrganizationName: (organizationName: string) => void;
   setOrganizationInitial: (organizationName: string) => void;
+  setIsSearching: (isSearching: boolean) => void;
+  setSearchText: (searchText: string) => void;
   fetchProducts: () => Promise<void>;
   fetchRevenue: () => Promise<void>;
   fetchMetrics: () => Promise<void>;
@@ -39,35 +46,42 @@ type State = {
 
 const getInitials = (name: string): string => {
   return name
-    .split(" ")
+    .split(' ')
     .map((word) => word.charAt(0).toUpperCase())
-    .join("");
+    .join('');
 };
 
 export const useStore = create<State>()(
   persist(
     (set) => ({
-      organizationId: "",
-      organizationName: "",
-      organizationInitial: "",
+      organizationId: '',
+      organizationName: '',
+      organizationInitial: '',
       products: [],
       isProductLoading: false,
       revenueData: [],
       isRevenueLoading: false,
       isRevenueError: false,
+      stockItems: [],
+      searchText: '',
+      isSearching: false,
+      isPremium: false,
+      setIsSearching: (isSearching) => set({ isSearching }),
+      setSearchText: (searchText) => set({ searchText }),
       metrics: [],
       isMetricsLoading: false,
       setOrganizationId: (organizationId) => set({ organizationId }),
       setOrganizationName: (organizationName) => set({ organizationName }),
-      setOrganizationInitial: (organizationName) => set({ organizationInitial: getInitials(organizationName) }),
+      setOrganizationInitial: (organizationName) =>
+        set({ organizationInitial: getInitials(organizationName) }),
       fetchProducts: async () => {
         set({ isProductLoading: true });
         try {
-          const res = await fetch("/api/product/get");
+          const res = await fetch('/api/product/get');
           const data = await res.json();
           set({ products: data });
         } catch (error) {
-          console.error("Failed to fetch products", error);
+          console.error('Failed to fetch products', error);
         } finally {
           set({ isProductLoading: false });
         }
@@ -75,11 +89,11 @@ export const useStore = create<State>()(
       fetchRevenue: async () => {
         set({ isRevenueLoading: true, isRevenueError: false });
         try {
-          const res = await fetch("/api/reports/sales/generate");
+          const res = await fetch('/api/reports/sales/generate');
           const data = await res.json();
           set({ revenueData: data?.weeklyRevenue || [] });
         } catch (error) {
-          console.error("Failed to fetch revenue", error);
+          console.error('Failed to fetch revenue', error);
           set({ isRevenueError: true });
         } finally {
           set({ isRevenueLoading: false });
@@ -88,52 +102,51 @@ export const useStore = create<State>()(
       fetchMetrics: async () => {
         set({ isMetricsLoading: true });
         try {
-          const res = await fetch("/api/report/metrics");
+          const res = await fetch('/api/report/metrics');
           const data = await res.json();
           const transformedMetrics = [
             {
-              title: "Total Revenue",
+              title: 'Total Revenue',
               value: data.totalRevenue,
               change: data.revenueChange,
-              icon: "/icons/revenue.svg",
-              textColor: "text-grey-600",
+              icon: '/icons/revenue.svg',
+              textColor: 'text-grey-600',
             },
             {
-              title: "Gross Profit Margin",
+              title: 'Gross Profit Margin',
               value: data.grossProfit,
               change: data.grossChange,
-              icon: "/icons/gross.svg",
-              textColor: "text-grey-600",
+              icon: '/icons/gross.svg',
+              textColor: 'text-grey-600',
             },
             {
-              title: "Stock Turnover Rate",
+              title: 'Stock Turnover Rate',
               value: data.stockTurnover,
               change: data.stockChange,
-              icon: "/icons/stockturn.svg",
-              textColor: "text-gray-600",
+              icon: '/icons/stockturn.svg',
+              textColor: 'text-gray-600',
             },
             {
-              title: "Total Sale Transaction",
+              title: 'Total Sale Transaction',
               value: data.totalSales,
               change: data.salesChange,
-              icon: "/icons/TotalSale.svg",
-              textColor: "text-grey-600",
+              icon: '/icons/TotalSale.svg',
+              textColor: 'text-grey-600',
             },
           ];
           set({ metrics: transformedMetrics });
         } catch (error) {
-          console.error("Failed to fetch metrics:", error);
+          console.error('Failed to fetch metrics:', error);
         } finally {
           set({ isMetricsLoading: false });
         }
       },
     }),
     {
-      name: "organization-store",
+      name: 'organization-store',
     }
   )
 );
-
 
 /*
 Usage for Stephen
